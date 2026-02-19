f7a4d0a2ad18bd1857aa5443266b5b6e7ae814d0
KT <s6804062612021@email.kmutnb.ac.th>
2026-02-19 21:20:10 +0700

    x Matrices

---
+++ lab9no5.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#define NMAX 10
+
+//functions prototype
+void inputArray(int array[], int N);
+void showArray2D(int matrix[][10], int N);
+void constructMatrix(int P[][10], int N, int A[ ], int B[ ]);
+
+int main() {
+   int a[NMAX], b[NMAX], p[NMAX][NMAX], n;
+   printf("Enter N = ");
+   scanf("%d", &n);
+   n = (n>10)? 10 : (n<1)? 1 : n;
+   printf("Input array A \n");
+   inputArray(a,n);
+   printf("Input array B \n");
+   inputArray(b,n);
+   constructMatrix(p, n, a, b);
+   printf("Matrix P \n");
+   showArray2D(p, n);
+   return 0;
+}
+
+void inputArray(int array[], int N){
+	printf("Enter %d integers: ",N);
+	for(int i=0;i<N;i++){
+		scanf("%d", &array[i]);
+	}
+}
+
+void showArray2D(int matrix[][10], int N){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			printf("%4d",matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void constructMatrix(int P[][10], int N, int A[], int B[]){
+	for(int i=0;i<N;i++){
+		int a = A[i];
+		for(int j=0;j<N;j++) P[i][j]=a*B[j];
+	}
+}
+
